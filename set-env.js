05dd9c7c3391ef41ba67910198fa9b6ea6536dad
Antonin <arzelantonin@gmail.com>
2026-04-27 12:11:37 +0200

    test

--- set-env.js
+++ set-env.js
@@ -9,6 +9,7 @@ export const environment = {
   production: true,
   supabaseUrl: '${process.env.SUPABASE_URL}',
   supabaseKey: '${process.env.SUPABASE_KEY}'
+  linkPreviewKey: '${process.env.LINK_PREVIEW_KEY}'
 };
 `;
 
